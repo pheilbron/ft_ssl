b218362208e6185e18b06abcae74b702bd3b7983
Phillip Heilbron <phillip.heilbron@gmail.com>
2019-10-02 09:25:15 -0700

    Updte ft_ssl.h

--- src/cipher/parse_cipher.c
+++ src/cipher/parse_cipher.c
@@ -12,6 +12,10 @@
 
 #include "ft_ssl.h"
 
+static int	(*set_cipher_context(char op))(t_ssl_context *, char **, int, int *)
+{
+	
+
 static int	parse_cipher_options(t_ssl_context *c, char **data, int len, int *i)
 {
 	int	data_i;
@@ -19,13 +23,14 @@ static int	parse_cipher_options(t_ssl_context *c, char **data, int len, int *i)
 	while (*i < len && data[*i][0] == '-')
 	{
 		data_i = 1;
-		while (data[*i][data_i])
-		{
-			if (set_ssl_option(c, data[*i][data_i++], e) < 0)
+		if (data[*i][data_i] && data[*i][data_i] == '-')
+			if (set_ssl_long_option(c, data[*i] + ++data_i) < 0)
 				return (c->e.no);
-			else
-				(*set_cipher_context(
-		}
+		while (data[*i][data_i])
+			if ((set_ssl_option(c, data[*i][data_i++]) < 0) ||
+				((*set_cipher_context(data[*i][data_i]))(c, data,
+					len, &(++(*i))) < 0))
+				return (c->e.no);
 		(*i)++;
 	}
 	return (c->e.no = 1);
