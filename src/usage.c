9c1cd477fecb47305d9960b2c547ecc642dde871
Phillip Heilbron <phillip.heilbron@gmail.com>
2019-11-20 10:02:43 -0800

    Update.

--- src/usage.c
+++ src/usage.c
@@ -22,12 +22,12 @@ int	print_usage(t_ssl_context c)
 	s = ft_dstr_init();
 	if (c.algorithm.type.a.family == MD)
 		ft_printf("usage: ft_ssl %s [-%s] [-s string] [files ...]\n",
-				(c.algorithm.type.a.algo ? c.algorithm.name : "command"),
+				(c.algorithm.type.algo ? c.algorithm.name : "command"),
 				get_ssl_options(s, c.algorithm.type)->buf);
-	else if (c.algorithm.type.a.family == CIPHER)
+	else if (c.algorithm.type.family == CIPHER)
 		ft_printf("usage: ft_ssl %s [-%s] %s[-i file] [-o file]\n",
-				c.algorithm.type.a.algo ? c.algorithm.name : "command",
-				c.algorithm.type.a.algo != BASE64 ? "" :
+				c.algorithm.type.algo ? c.algorithm.name : "command",
+				c.algorithm.type.algo != BASE64 ? "" :
 				"[-k key] [-p password] [-s salt] [-v init_vector] ",
 				get_ssl_options(s, c.algorithm.type)->buf);
 	else
